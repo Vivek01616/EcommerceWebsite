a4901d8d4e56edbbad3e51fc3387a19081fc5cbc
Vivek <nanivivek016@gmail.com>
2024-07-21 18:57:38 +0530

    Fixed bugs and added new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SigninComponent } from './Frontend/signin/signin.component';
+import {SignupComponent} from './Frontend/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SigninComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
